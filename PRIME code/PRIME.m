8d366d47b9fcefeee53c48cd2f8d78f9329ab750
Selva Rupa Christinal Immanuel <christinaias87@gmail.com>
2021-01-27 15:31:04 -0800

    Update PRIME.m
    Updated the comment outs for clear understanding

--- PRIME code/PRIME.m
+++ PRIME code/PRIME.m
@@ -1,56 +1,56 @@
-function [RegulatedModel, F_KO, TFs, RefineValue] = PRIME (model, Regulator, Target, Magnitude, GeneExpressionIDs, GeneExpression, overallExpressionid, minOverallExpression, maxOverallExpression)
+function [RegulatedModel, F_KO, TFs] = PRIME (model, Regulator, Target, Magnitude, GeneExpressionIDs, GeneExpression, overallExpressionid, minOverallExpression, maxOverallExpression)
 %%
-%PRIME is used to integrate regulatory network onto metabolic networks
-%It predicts the phenotype of TF influences
-%It also gives the pertubed models as output
-%PRIME calculates the magnitude of TF influences on the genes and reactions
+%PRIME is an algorithm used to integrate regulatory network onto metabolic networks
+%It predicts the phenotype of TF and gene knockouts
+%It also gives the perturbed models as output
+%PRIME calculates the magnitude of TF influences on the genes and their associated reactions
 %
 %USAGE:
-%   [RegulatedModel, F_KO, F_OE, TFs] = IDREAM (model, Regulator, Target, Probability, Magnitude, Litevidence)
+%   [RegulatedModel, F_KO, TFs] = PRIME (model, Regulator, Target,Magnitude, GeneExpressionIDs, GeneExpression, overallExpressionid, minOverallExpression, maxOverallExpression);
 %
 %INPUT:
-%   model:                  Metabolic network as COBRA structure (with expression data applied using iMAT/GIMME)
-%   Regulator and Target:   Cell arrays listing the TF-target gene interactions. 
+%   model:                  Metabolic network as COBRA structure (with expression data applied using iMAT/GIMME) e.g., iEK1011
+%   Regulator and Target:   Cell arrays listing the TF-target gene interactions. Regulators = {list of TFs}; Targets = {list of gene targets};
 %                           Regulators and Targets have the same number of rows; each row describes a separate interaction.
-%   Magnitude:              Weights (Beta) from INFERELATOR run of EGRIN.
-%   GeneExpressionIDs       Gene ID of GE data (present study)
+%   Magnitude:              Weights (Beta) from INFERELATOR run of EGRIN. Magnitude has the same number of rowss as Regulators and Targets
+%   GeneExpressionIDs       Gene ID of GE data (present study) e.g., isoniazid treatment
 %   Gene Expression         GE data corresponding to the GeneExpressionIDs (present study)
 %   overallExpressionid     Gene ID of GE data (Gene expression from various conditions)
 %   minOverallExpression    Min value of GE data from various conditions
 %   maxOverallExpression    Max value of GE data from various conditions
 %
 %OUTPUT:
-%   RegulatedModel  TF pertubed model.
+%   RegulatedModel  TF pertubed model (Models of TF KOs)
 %   F_KO:           Growth rate of Knockouts (Objective function of the model)
 %   TFs:            List of TFs being knocked out or over expressed
-%   RefineValue     ReFInE calculated for the present study
 %
 % .. Authors:
 %   - Selva Rupa Christinal Immanuel, ISB, Seattle, USA. 05/31/2019 Original code
-%%
+%% COBRA toolbox intialization
 xx=waitbar(0,'PRIME in progress ...');
 %disp('COBRA toolbox initialization...')
 %initCobraToolbox
 %changeCobraSolver('glpk');
-%%
+%% INPUT processing for PRIME run
 disp('processing inputs...')
 threshold = 10^(-6);
 [uniqueTFs] = unique(Regulator);
 model = buildRxnGeneMat(model);
 waitbar(0.15,xx,'PRIME in progress ...');
 disp('running Flux Variability Analysis...')
-[minFlux, maxFlux] = fluxVariability(model);
+[minFlux, maxFlux] = fluxVariability(model); %gives the best flux span for the condition
+%minFlux = model.lb; %default minFlux
+%maxFlux = model.ub; %default maxFlux
 minFlux(abs(minFlux) < threshold) = 0;
 maxFlux(abs(maxFlux) < threshold) = 0;
-%Magnitude = (Magnitude-min(Magnitude))/(max(Magnitude)-min(Magnitude));
+Magnitude = (Magnitude-min(Magnitude))/(max(Magnitude)-min(Magnitude)); %rank the magnitude value to compare the models accrosss conditions
 %%
 disp('initializing PRIME...')
 waitbar(0.33,xx,'PRIME in progress ...');
 %objective = [find(model.c) obj_frac]; 
 RxnNumber = size(model.S,2);
 %parsedGPR = GPRparser(model);
-
-%%
+%% Running PRIME
 %Apply Refine value of each TF perturbation to its corresponding reactions 
 TFs = uniqueTFs;
 genelist = ismember(Target,model.genes);
@@ -73,8 +73,8 @@ for n = 1:length(model.genes)
     genePos = ismember(Target,model.genes(n));
     geneMagnitude = Magnitude(genePos);
     %geneMagnitude = (geneMagnitude - min(geneMagnitude))/(max(geneMagnitude)-min(geneMagnitude));
-    %sumgeneBetas(n,1) = sum(abs(geneMagnitude));
-    sumgeneBetas(n,1) = sum(geneMagnitude);
+    sumgeneBetas(n,1) = sum(abs(geneMagnitude));
+    %sumgeneBetas(n,1) = sum(geneMagnitude);
 end
 waitbar(0.75,xx,'PRIME in progress ...');    
 disp('mapping ReFInE to maxFlux of Reactions...')
@@ -135,9 +135,9 @@ for m = 1:length(TFs)
                     end
     RegulatedModel{m,1} = model;
     inhibit = RegulatedModel{m,1}.c;
-    RegulatedModel{m,1}.c = inhibit.*min(abs(Refine)); %weight factor
-    RegulatedModel{m,1}.ub = maxFlux.*Refine;%max flux perturbation
-    RegulatedModel{m,1}.lb = minFlux.*Refine;%min flux perturbation
+    RegulatedModel{m,1}.c = inhibit.*min(abs(Refine)); % new weight factor
+    RegulatedModel{m,1}.ub = maxFlux.*Refine;%max flux perturbation using Refine
+    RegulatedModel{m,1}.lb = minFlux.*Refine;%min flux perturbation using Refine
     ubound = RegulatedModel{m,1}.ub;
     lbound = RegulatedModel{m,1}.lb;
                     for x = 1:RxnNumber
